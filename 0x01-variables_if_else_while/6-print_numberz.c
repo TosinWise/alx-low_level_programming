884515adb4a4323609e298ab302067962b49e835
TosinWise <anuoluwapo3579@gmail.com>
2022-10-14 15:52:09 -0700

    6 of C

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints the lowercase alphabet in reverse
+ * main - Prints the lowercase alphabet in reverse.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -11,7 +11,7 @@ int main(void)
 
 	for (letter = 'z'; letter >= 'a'; letter--)
 		putchar(letter);
-	
+
 	putchar('\n');
 
 	return (0);
