c9a3b2dbc1964e051fbdd29a4330f100e3ce5df2
TosinWise <anuoluwapo3579@gmail.com>
2022-10-27 22:55:51 -0700

    ALongTime

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,11 +1,24 @@
-#ifndef main_h
-#define main_h	
-
 #include <stdio.h>
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
 
-#endif /* main_h */
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+/*
+*while (i < 10)
+*{
+*putchar(i);
+*}
+*/
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
